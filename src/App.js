af1b94c219e050cea2aa83ded8956eecb74b9253
Alexander Schelchere <ascellie@gmail.com>
2019-01-19 17:07:44 -0500

    Really empty skeleton of our app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ class App extends Component {
     render() {
         return (
         <div className="content">
-            <h1>Wise Weather</h1>
+            <header className="header">
+                <h1>Am I doing a thing</h1>
+            </header>
+            <main>
+                <section className="section-weather-today">
+                    <p>Component holding the weather data for today will go here</p>
+                </section>
+                <section className="section-weather-week">
+                    <p>Component holding the weather data for the week will go here.</p>
+                </section>
+            </main>
         </div>
         )
     }
