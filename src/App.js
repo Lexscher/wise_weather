be2b80a7a42cc7c2ad07a79a8faa5958b463b116
Alexander Schelchere <ascellie@gmail.com>
2019-01-24 17:51:45 -0500

    App.js - Class turned into a function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './sass/main.scss';
+require('./assets/favicon.png');
 
-class App extends Component {
-    render() {
-        return (
-        <div className="content">
-            <header className="header">
-                <h1>Am I doing a thing</h1>
-            </header>
-            <main>
-                <section className="section-weather-today">
-                    <p>Component holding the weather data for today will go here</p>
-                </section>
-                <section className="section-weather-week">
-                    <p>Component holding the weather data for the week will go here.</p>
-                </section>
-            </main>
-        </div>
-        )
-    }
+const App = () => {
+    return (
+    <div className="content">
+        <header className="header">
+            <h1>Am I doing a thing</h1>
+        </header>
+        <main>
+            <section className="section-weather-today">
+                <p>Component holding the weather data for today will go here</p>
+            </section>
+            <section className="section-weather-week">
+                <p>Component holding the weather data for the week will go here.</p>
+            </section>
+        </main>
+    </div>
+    )
 }
 
 export default App;
